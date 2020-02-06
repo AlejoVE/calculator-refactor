56412c88878cdfeb337211645f7631c7d8ddd243
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-02-06 11:29:52 +0100

    I added the javascprit code in logic
    I also added comments in handler and added the logic link to my html

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,7 +1,11 @@
 function calculateHandler(event) {
+
+  // read & process user input from event
   var a = parseInt(document.querySelector("#value1").value);
   var b = parseInt(document.querySelector("#value2").value);
   var op = document.querySelector("#operator").value;
+
+  // pass user input through core logic
   var calculate;
 
   if (op === "add"){
@@ -17,7 +21,12 @@ function calculateHandler(event) {
     calculate = a * b;
   }
 
+   // render output to DOM for user
   document.querySelector("#result").innerHTML = calculate;
+
+  // log user action for developers
+  console.log(calculate);
   
+   // return true for the browser
   return true;
 }
